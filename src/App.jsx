281a19ca2ee4758fec30c0c6f94c9c166d298630
Scott <scott-h@shaw.ca>
2024-05-27 18:24:04 +0000

    reorg cube

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 
-import { useState } from 'react'
 import { useEffect } from "react"
 import Profile from './components/profile'
 import Info from './components/info'
 import Cube from './components/cube'
+import { makeCubes } from './components/cube';
 import 'bootstrap/dist/css/bootstrap.css'
 import $ from 'jquery'
 
@@ -16,12 +16,19 @@ import './App.css'
 function App() {
   // const [count, setCount] = useState(0)
 
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+
   useEffect(() => {
     // console.log("logging this here")
 
     const vidEle = $($.parseHTML('<video autoplay muted id="dropVid"><source src="' + Cloud + '" type="video/mp4"></video>'))
     $("body").prepend(vidEle)
-    $("body").prepend('<div id="cubeStore"></div><div id="background" class="background"> <img src="'+imgs[0]+'" className="mont" /></div>')
+    $("body").prepend('<div id="cubeStore"></div><div id="background" class="background"> <img src="' + imgs[0] + '" className="mont" /></div>')
 
     setTimeout(function () {
       $('#main').addClass("mainShow")
@@ -33,6 +40,115 @@ function App() {
 
     // const images = require.context('./assets', true);
     // console.log({images})
+
+    function grav() {
+
+      $("#background").clone().insertAfter('#background').prop('id', 'backgroundGrav')
+      $("#root").clone().appendTo("#backgroundGrav").prop('id', 'rootGrav')
+      $("#rootGrav #main").prop('id', 'rootMain')
+
+      $("#main").css({ color: "black" })
+
+      var r1 = (Math.random())
+      var r2 = (Math.random())
+      var r3 = (Math.random())
+      const rotations = 160;
+
+      const eWidth = $(window).width();
+      const eHeight = $(window).height();
+
+      $("#background").css({
+        "transition": "opacity 0s",
+        "opacity": "0"
+      });
+
+      $("#background>img").removeClass("zoomed")
+
+      $("#backgroundGrav").css({
+        "transition": "transform 8s, opacity 6s",
+        "opacity": "0",
+        "transform":
+          "translate(" + ((r1 - 0.5) * eWidth).toString() + "px, " + ((r2 - 0.5) * eHeight).toString() + "px) scale(" + (0).toString() + ") rotate3d(" + (r1 * 2 - 1).toString() + ", " + (r2 * 2 - 1).toString() + ", " + (r3 * 2 - 1).toString() + ", " + (rotations).toString() + "deg) "
+        // "transform-origin": "center"
+      });
+
+      setTimeout(function () {
+        $('#backgroundGrav').remove()
+        $("#background>img").removeClass("background-change")
+        $("#background>img").removeClass("zoomed")
+      }, 6000)
+
+    }
+
+    function fadeBack() {
+      //fade rand background into view
+
+      const imgIndex = getRandomInt(0, imgs.length - 1)
+      $("#background>img").attr({ "src": imgs[imgIndex] })
+
+      $("#background").css({
+        "transition": "opacity 8s",
+        "opacity": ".7"
+
+      })
+
+      $("#background>img").addClass("zoomed")
+      // $("#backgroundGrav>img")
+    }
+
+    function sepChange() {
+      //change background filter
+      $("#background>img").addClass("background-change")
+
+      // $("#backgroundGrav>img.zoomed").css({"transform": "scale(1.1)"})
+    }
+
+    function vidDrop() {
+
+      $("#dropVid").addClass('dropVid-changed')
+      $('#dropVid').prop({
+        currentTime: 0
+      });
+
+      $('#dropVid').prop({
+        playbackRate: 0.8
+      });
+
+      $('#dropVid')[0].play();
+
+      setTimeout(function () {
+
+        $("#dropVid").removeClass('dropVid-changed')
+
+      }, 7000)
+    }
+
+
+    function loop() {
+
+      setTimeout(function () {
+        makeCubes()
+      }, 10000)
+
+      setTimeout(function () {
+        fadeBack()
+      }, 40000)
+
+      setTimeout(function () {
+        sepChange()
+      }, 60000)
+
+      setTimeout(function () {
+        grav()
+      }, 80000)
+
+      setTimeout(function () {
+        vidDrop()
+      }, 90000)
+
+    }
+    loop()
+    setInterval(loop, 100000)
 
   }, [])
 
--- src/components/cube.jsx
+++ src/components/cube.jsx
@@ -1,264 +1,7 @@
-// import Mont from '../assets/pexels-eberhardgross-1062249.jpg';
-// import Cloud from '../assets/1108225_1080p_Pines_Valley_1280x720.mp4';
-import imgs from '../components/backgrounds';
+
 import $ from 'jquery';
 
 const Cube = () => {
-
-    function getRandomInt(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-
-    const makeCubes = async (type) => {
-        var cubes = []
-
-        function setupSwarmElements(elementId, n) {
-
-            const duration = 20000
-            const rotations = 15
-
-            for (let j = 0; j < n; j++) {
-                var pEle = $("#cubeStore");
-                var eleObj = $("#cube").clone().appendTo(pEle)
-                eleObj.removeAttr("id");
-                eleObj.attr("id", "cube_" + j);
-                eleObj.addClass("cube-clone")
-                if (j !== 0) {
-                    // eleObj.find(".heart").remove();
-                }
-
-                var r1 = (Math.random())
-                var r2 = (Math.random())
-                var r3 = (Math.random())
-
-                var r4 = (Math.random())
-                var r5 = (Math.random())
-                var r6 = (Math.random())
-
-                var r7 = (Math.random())
-                var r8 = (Math.random())
-                var r9 = (Math.random())
-                var startTime = new Date();
-
-                var colorpicks = ["rgba(54,122,224,", "rgba(224,156,54,", "rgba(224,71,54,", "rgba(122,224,54,", "rgba(224,54,122,"];
-                var rndInt = getRandomInt(0, 4)
-
-                cubes.push({ "id": "cube_" + j, "r1": r1, "r2": r2, "r3": r3, "r4": r4, "r5": r5, "r6": r6, "r7": r7, "r8": r8, "r9": r9, "color": colorpicks[rndInt], "startTime": startTime, "duration": duration, "rotations": rotations })
-            }
-
-            setTimeout(function () {
-                window.requestAnimationFrame(moveElements)
-            }, 200)
-        }
-
-        function moveElements(timeStamp) {
-            frame++;
-            var thisFrameLag = frameLag - 16;
-            if (thisFrameLag < 0) {
-                thisFrameLag = 0
-            }
-            if (thisFrameLag > 100) {
-                thisFrameLag = 100
-            }
-            if (frame > 3 + (thisFrameLag)) {
-                //console.log(frame, thisFrameLag);
-                frame = 0;
-
-                for (let i in cubes) {
-                    var cube = cubes[i]
-                    const timeRatio = ((new Date().getTime() - new Date(cube.startTime).getTime()) / cube.duration)
-                    const opacityCurve = (Math.sin(Math.PI * (timeRatio * 2 - .5)) + 1) / 2
-
-                    // console.log(timeRatio, Math.sin(Math.PI * (timeRatio * 2 - .5)), opacityCurve)
-
-                    let r1 = cube.r1 + (cube.r4 - cube.r1) * timeRatio;
-                    let r2 = cube.r2 + (cube.r5 - cube.r2) * timeRatio;
-                    let r3 = cube.r3 + (cube.r6 - cube.r3) * timeRatio;
-                    let r7 = cube.r7
-                    let r8 = cube.r8
-                    let r9 = cube.r9
-
-                    const rotations = cube.rotations * 360 * r3
-                    // let scrollHeight = $("#main").scrollTop() * (1 - r3);
-
-                    var eleObj = $("#" + cube.id)
-                    var eWidth = eleObj.parent().width() * 0.95;
-
-                    var eHeight = $(window).height();
-                    // eleObj.show();
-
-                    eleObj.css({
-                        "-webkit-backface-visibility": "initial",
-                        // "transform": "translate(" + (r1 * eWidth).toString() + "px, " + (r2 * eHeight - eHeight / 2).toString() + "px) scale(" + (r3 * 2 + .1).toString() + ") rotate3d(" + (r7 * 2 - 1).toString() + ", " + (r8 * 2 - 1).toString() + ", " + (r9 * 2 - 1).toString() + ", " + (rotations).toString() + "deg) ",
-                        // "transform": "translate(" + (r1 * eWidth).toString() + "px, " + (r2 * eHeight - eHeight / 2).toString() + "px) scale(" + (r3).toString() + ") rotate3d(" + (r7 * 2 - 1).toString() + ", " + (r8 * 2 - 1).toString() + ", " + (r9 * 2 - 1).toString() + ", " + (rotations).toString() + "deg) ",
-                        "transform": "translate(" + (r1 * eWidth).toString() + "px, " + (r2 * eHeight).toString() + "px) scale(" + (r3 * 1.5).toString() + ") rotate3d(" + (r7 * 2 - 1).toString() + ", " + (r8 * 2 - 1).toString() + ", " + (r9 * 2 - 1).toString() + ", " + (rotations).toString() + "deg) ",
-                        // "transform": "translate(" + ((r1 * 2 - 1) * eWidth).toString() + "px, " + ((r2 * 2 - 1) * eHeight).toString() + "px) scale(" + (r3).toString() + ") rotate3d(" + (r7 * 2 - 1).toString() + ", " + (r8 * 2 - 1).toString() + ", " + (r9 * 2 - 1).toString() + ", " + (rotations).toString() + "deg) ",
-
-                        "transform-origin": "center"
-                    });
-
-                    eleObj.find(".cube-face").css(
-                        {
-                            "border-color": "rgba(0, 0, 0," + (opacityCurve / 8 * (r3)).toString() + ")",
-                            "background-color": cube.color + (opacityCurve / 8 * (r3)).toString() + ")"
-                        }
-                    );
-                    // eleObj.find(".heart").css(
-                    //     {
-                    //         "opacity": (r3 / 6).toString()
-                    //     }
-                    // )
-                }
-
-                frameLag = timeStamp - lastTimeStamp;
-                lastTimeStamp = timeStamp;
-                if ((new Date().getTime() - new Date(cube.startTime).getTime()) < cube.duration) {
-                    window.requestAnimationFrame(moveElements);
-                } else {
-                    for (let i in cubes) {
-                        var cube = cubes[i]
-                        var eleObj = $("#" + cube.id)
-                        // if( i == 0 ){
-                        eleObj.remove()
-                        // }
-                    }
-                    cubes = [];
-                }
-            } else {
-                frameLag = timeStamp - lastTimeStamp;
-                lastTimeStamp = timeStamp;
-                window.requestAnimationFrame(moveElements);
-            }
-        }
-
-        var frame = 0;
-        var lastTimeStamp = new Date().getTime();
-        var frameLag = 16;
-
-        setupSwarmElements("cube", 20);
-    }
-
-    function grav() {
-
-        $("#background").clone().insertAfter('#background').prop('id', 'backgroundGrav')
-        $("#root").clone().appendTo("#backgroundGrav").prop('id', 'rootGrav')
-        $("#rootGrav #main").prop('id', 'rootMain')
-
-        $("#main").css({ color: "black" })
-
-        var r1 = (Math.random())
-        var r2 = (Math.random())
-        var r3 = (Math.random())
-        const rotations = 160;
-
-        const eWidth = $(window).width();
-        const eHeight = $(window).height();
-
-        $("#background").css({
-            "transition": "opacity 0s",
-            "opacity": "0"
-        });
-
-        $("#background>img").removeClass("zoomed")
-
-        $("#backgroundGrav").css({
-            "transition": "transform 8s, opacity 6s",
-            "opacity": "0",
-            "transform":
-                "translate(" + ((r1 - 0.5) * eWidth).toString() + "px, " + ((r2 - 0.5) * eHeight).toString() + "px) scale(" + (0).toString() + ") rotate3d(" + (r1 * 2 - 1).toString() + ", " + (r2 * 2 - 1).toString() + ", " + (r3 * 2 - 1).toString() + ", " + (rotations).toString() + "deg) "
-            // "transform-origin": "center"
-        });
-
-        setTimeout(function () {
-            $('#backgroundGrav').remove()
-            $("#background>img").removeClass("background-change")
-            $("#background>img").removeClass("zoomed")
-        }, 6000)
-
-    }
-
-    function fadeBack() {
-        //fade rand background into view
-
-        const imgIndex=getRandomInt(0, imgs.length-1)
-        $("#background>img").attr({"src":imgs[imgIndex]})
-
-        $("#background").css({
-            "transition": "opacity 8s",
-            "opacity": ".7"
-
-        })
-
-        $("#background>img").addClass("zoomed")
-        // $("#backgroundGrav>img")
-    }
-
-    function sepChange() {
-        //change background filter
-        $("#background>img").addClass("background-change")
-
-        // $("#backgroundGrav>img.zoomed").css({"transform": "scale(1.1)"})
-    }
-
-    function vidDrop() {
-
-        $("#dropVid").addClass('dropVid-changed')
-        $('#dropVid').prop({
-            currentTime: 0
-        });
-
-
-        // css({
-        //     'transition': 'transform 15s, opacity 7s',
-        //     'transform': 'scale(1)',
-        //     'opacity': '1'
-        // })
-
-        // document.querySelector('dropVid').playbackRate = 0.2;
-        $('#dropVid').prop({
-            playbackRate: 0.8
-        });
-
-        // setTimeout(function () {
-        $('#dropVid')[0].play();
-        // }, 2000)
-
-        setTimeout(function () {
-            
-            $("#dropVid").removeClass('dropVid-changed')
-
-        }, 7000)
-    }
-
-
-    function loop() {
-
-        setTimeout(function () {
-            makeCubes()
-        }, 10000)
-
-        setTimeout(function () {
-            fadeBack()
-        }, 40000)
-
-        setTimeout(function () {
-            sepChange()
-        }, 60000)
-
-        setTimeout(function () {
-            grav()
-        }, 80000)
-       
-        setTimeout(function () {
-            vidDrop()
-        }, 90000)
-
-    }
-    loop()
-    setInterval(loop, 100000)
-
     return (
         <div>
             <div id="cube-container"><div id="cube" className="cube">
@@ -273,5 +16,142 @@ const Cube = () => {
     )
 }
 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const makeCubes = async (type) => {
+    // function makeCubes() {
+    var cubes = []
+
+    function setupSwarmElements(elementId, n) {
+
+        const duration = 20000
+        const rotations = 15
+
+        for (let j = 0; j < n; j++) {
+            var pEle = $("#cubeStore");
+            var eleObj = $("#cube").clone().appendTo(pEle)
+            eleObj.removeAttr("id");
+            eleObj.attr("id", "cube_" + j);
+            eleObj.addClass("cube-clone")
+            if (j !== 0) {
+                // eleObj.find(".heart").remove();
+            }
+
+            var r1 = (Math.random())
+            var r2 = (Math.random())
+            var r3 = (Math.random())
+
+            var r4 = (Math.random())
+            var r5 = (Math.random())
+            var r6 = (Math.random())
+
+            var r7 = (Math.random())
+            var r8 = (Math.random())
+            var r9 = (Math.random())
+            var startTime = new Date();
+
+            var colorpicks = ["rgba(54,122,224,", "rgba(224,156,54,", "rgba(224,71,54,", "rgba(122,224,54,", "rgba(224,54,122,"];
+            var rndInt = getRandomInt(0, 4)
+
+            cubes.push({ "id": "cube_" + j, "r1": r1, "r2": r2, "r3": r3, "r4": r4, "r5": r5, "r6": r6, "r7": r7, "r8": r8, "r9": r9, "color": colorpicks[rndInt], "startTime": startTime, "duration": duration, "rotations": rotations })
+        }
+
+        setTimeout(function () {
+            window.requestAnimationFrame(moveElements)
+        }, 200)
+    }
+
+    function moveElements(timeStamp) {
+        frame++;
+        var thisFrameLag = frameLag - 16;
+        if (thisFrameLag < 0) {
+            thisFrameLag = 0
+        }
+        if (thisFrameLag > 100) {
+            thisFrameLag = 100
+        }
+        if (frame > 3 + (thisFrameLag)) {
+            //console.log(frame, thisFrameLag);
+            frame = 0;
+
+            for (let i in cubes) {
+                var cube = cubes[i]
+                const timeRatio = ((new Date().getTime() - new Date(cube.startTime).getTime()) / cube.duration)
+                const opacityCurve = (Math.sin(Math.PI * (timeRatio * 2 - .5)) + 1) / 2
+
+                // console.log(timeRatio, Math.sin(Math.PI * (timeRatio * 2 - .5)), opacityCurve)
+
+                let r1 = cube.r1 + (cube.r4 - cube.r1) * timeRatio;
+                let r2 = cube.r2 + (cube.r5 - cube.r2) * timeRatio;
+                let r3 = cube.r3 + (cube.r6 - cube.r3) * timeRatio;
+                let r7 = cube.r7
+                let r8 = cube.r8
+                let r9 = cube.r9
+
+                const rotations = cube.rotations * 360 * r3
+                // let scrollHeight = $("#main").scrollTop() * (1 - r3);
+
+                var eleObj = $("#" + cube.id)
+                var eWidth = eleObj.parent().width() * 0.95;
+
+                var eHeight = $(window).height();
+                // eleObj.show();
+
+                eleObj.css({
+                    "-webkit-backface-visibility": "initial",
+                    // "transform": "translate(" + (r1 * eWidth).toString() + "px, " + (r2 * eHeight - eHeight / 2).toString() + "px) scale(" + (r3 * 2 + .1).toString() + ") rotate3d(" + (r7 * 2 - 1).toString() + ", " + (r8 * 2 - 1).toString() + ", " + (r9 * 2 - 1).toString() + ", " + (rotations).toString() + "deg) ",
+                    // "transform": "translate(" + (r1 * eWidth).toString() + "px, " + (r2 * eHeight - eHeight / 2).toString() + "px) scale(" + (r3).toString() + ") rotate3d(" + (r7 * 2 - 1).toString() + ", " + (r8 * 2 - 1).toString() + ", " + (r9 * 2 - 1).toString() + ", " + (rotations).toString() + "deg) ",
+                    "transform": "translate(" + (r1 * eWidth).toString() + "px, " + (r2 * eHeight).toString() + "px) scale(" + (r3 * 1.5).toString() + ") rotate3d(" + (r7 * 2 - 1).toString() + ", " + (r8 * 2 - 1).toString() + ", " + (r9 * 2 - 1).toString() + ", " + (rotations).toString() + "deg) ",
+                    // "transform": "translate(" + ((r1 * 2 - 1) * eWidth).toString() + "px, " + ((r2 * 2 - 1) * eHeight).toString() + "px) scale(" + (r3).toString() + ") rotate3d(" + (r7 * 2 - 1).toString() + ", " + (r8 * 2 - 1).toString() + ", " + (r9 * 2 - 1).toString() + ", " + (rotations).toString() + "deg) ",
+
+                    "transform-origin": "center"
+                });
+
+                eleObj.find(".cube-face").css(
+                    {
+                        "border-color": "rgba(0, 0, 0," + (opacityCurve / 8 * (r3)).toString() + ")",
+                        "background-color": cube.color + (opacityCurve / 8 * (r3)).toString() + ")"
+                    }
+                );
+                // eleObj.find(".heart").css(
+                //     {
+                //         "opacity": (r3 / 6).toString()
+                //     }
+                // )
+            }
+
+            frameLag = timeStamp - lastTimeStamp;
+            lastTimeStamp = timeStamp;
+            if ((new Date().getTime() - new Date(cube.startTime).getTime()) < cube.duration) {
+                window.requestAnimationFrame(moveElements);
+            } else {
+                for (let i in cubes) {
+                    var cube = cubes[i]
+                    var eleObj = $("#" + cube.id)
+                    // if( i == 0 ){
+                    eleObj.remove()
+                    // }
+                }
+                cubes = [];
+            }
+        } else {
+            frameLag = timeStamp - lastTimeStamp;
+            lastTimeStamp = timeStamp;
+            window.requestAnimationFrame(moveElements);
+        }
+    }
+
+    var frame = 0;
+    var lastTimeStamp = new Date().getTime();
+    var frameLag = 16;
+
+    setupSwarmElements("cube", 20);
+}
+
 export default Cube
+export { makeCubes }
 
