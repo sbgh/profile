551a47b57501d44583bd0f99f932797d40942bd1
Scott <scott-h@shaw.ca>
2024-10-11 22:11:24 +0000

    tech backgrounds

--- src/components/tech.jsx
+++ src/components/tech.jsx
@@ -9,9 +9,9 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-const title = 'These are some of the projects that we have worked on recently'
+const title = 'These are some of the projects that we have worked on'
 const techsList = `
-WordPress Custom theme (Professional Services)
+WordPress Custom Theme (Professional Services)
 Stylish WordPress theme with hand coded 3D effects and responsive design. Plugins include Advanced Custom Fields for content management and WP Mail SMTP for online messaging.
 https://kilanicorp.com/
 WordPress Custom Theme (Hospitality)
@@ -20,8 +20,8 @@ https://theinncalgary.com/
 dsStack Data Science Environment Builder
 Complex Graphical User Interface and Node.js server. This Free Open Source application has the ability to connect to cloud servers and provision them with packages used by data scientists. Things like Docker, Steamlit and more. 
 https://ezstacksystems.com:8443/
-ezStack Automated Web server Application Builder
-An even more complex Free Open Source application made with Node.js. Used to build web application, and manage servers, with a building-blocks approach. 
+ezStack Automated Web Server & Application Builder
+An even more complex Free Open Source application made with Node.js. Used to build web application, and manage servers, with a building-blocks approach. Provides user feedback via headless Chrome integration.
 https://github.com/sbgh/builder
 `
 
@@ -30,6 +30,7 @@ const techsArr = techsList.split("\n").filter(function (e) { return e }); //spli
 setTimeout(function () {
 
     $("#techsList").html("")
+    $("#techBack").html("")
 
     $("#techsTitle").text(title)
 
@@ -60,10 +61,8 @@ setTimeout(function () {
 
             if (x % 2 === 0) {
                 itemEle.find(".techItem").append(itemImgHTML)
-                // itemEle.find(".techItemText").addClass("offset-lg-2")
             } else {
                 itemEle.find(".techItem").prepend(itemImgHTML)
-                // itemEle.find(".techItemImg").addClass("offset-lg-2")
             }
 
 
@@ -71,37 +70,71 @@ setTimeout(function () {
             itemEle.find(".techItemImg").append(imgOb)
 
             $("#techsList").append(itemEle)
+            $("#techBack").append("<div class='techBack' style='background-image:url(" + sImg + ")'></div>")
 
-            // const numOfItems = twoDeeArr.length
-            // const sWidth = $("body").width()
-            // const lWidth = $("#techs").width()
-
-            // $(".techItem").each(function (index) {
-            //     let xTrans = lWidth / numOfItems * (index) * 7.5 + sWidth / 3
-            //     $( this ).css({"transform": "perspective(50px) translate3d("+ -xTrans +"px, -230px, -50px)"})
-            // });
-
+            $(".techBack").each(function (index) {
+                $(".techBack").eq(index).css({ "opacity": "0" })
+            })
 
         }
 
-        // var scroller = document.querySelector(".scroller")
-        // scroller.addEventListener('scrollsnapchanging', event => {
+        $("#techBack").append("<div class='techBackGlass'></div>")
 
-        //     if (event.snapTargetBlock) {
-        //         console.log(event.snapTargetBlock.attributes["idx"].value);
+        var scroller = document.querySelector(".scroller")
+        scroller.addEventListener('scrollsnapchange', event => {
 
-        //         $(".techItemHolder img").each(function (index) {
-        //             let thisIdx = event.snapTargetBlock.attributes["idx"].value
-        //             if (thisIdx == index.toString()) {
-        //                 $(event.snapTargetBlock).addClass("scrollsnapSelected")
-        //             } else {
-        //                 $(".techItemHolder img").eq(index).removeClass("scrollsnapSelected")
-        //             }
+            if (event.snapTargetBlock) {
+                let thisIdx = event.snapTargetBlock.attributes["idx"].value
+                console.log(thisIdx);
 
-        //         });
-        //     }
+                $("#techs").addClass("scrollsnapSelected")
 
-        // });
+                $(".techBack").each(function (index) {
+                    $(".techBack").eq(index).css({ "opacity": "0" })
+                })
+
+                $(".techBack").eq(thisIdx).css({ "opacity": ".3" })
+
+                // $(".techItemHolder img").each(function (index) {
+                //     let thisIdx = event.snapTargetBlock.attributes["idx"].value
+                //     if (thisIdx == index.toString()) {
+                //         $(event.snapTargetBlock).addClass("scrollsnapSelected")
+                //     } else {
+                //         $(".techItemHolder img").eq(index).removeClass("scrollsnapSelected")
+                //     }
+                // });
+            }
+
+        });
+
+        //setup observe on about and contactForm
+        let options = {
+            root: null,
+            threshold: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.00],
+            rootMargin: '-8% 0% -10% 0%',
+        };
+
+        let showTechs = function (entries, observer) {
+            entries.forEach(entry => {
+                const ratio = entry.intersectionRatio //entry.isIntersecting
+                //   entry.target.style.opacity = ratio
+
+                //   let ele = entry.target
+                if (ratio < .2) {
+                    $("#techs").removeClass("scrollsnapSelected")
+                    $(".techBack").each(function (index) {
+                        $(".techBack").eq(index).css({ "opacity": "0" })
+                    })
+                }
+
+            });
+        }
+
+        let observeShowTechs = new IntersectionObserver(showTechs, options)
+
+        let target = document.querySelector("#techs")
+        observeShowTechs.observe(target)
+
     }
 
 }, 300)
@@ -113,6 +146,7 @@ const Tech = () => {
     return (
         <div>
             <div id="techs" className="techs">
+                <div id="techBack" ></div>
                 <div id="techsTitle" className="techsTitle"></div>
                 <div id="techsList" className="techsList"></div>
             </div>
